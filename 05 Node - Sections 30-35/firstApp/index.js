fc9a15348c20bdb13ba7d0663e81a49cea69e0d2
manzonlucas <manzonlucasm@gmail.com>
2022-12-28 15:59:05 -0300

    express routing basics

--- 05 Node - Sections 30-35/firstApp/index.js
+++ 05 Node - Sections 30-35/firstApp/index.js
@@ -5,10 +5,33 @@ const app = express();
 //   console.log('request received');
 // });
 
-app.use((req, res) => {
-  res.send('Request received, this is a response')
-});
+// app.use((req, res) => {
+//   res.send('Request received, this is a response')
+// });
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
+})
+
+// GETs
+app.get('/', (req, res) => {
+  res.send('Welcome, this is home')
+})
+
+app.get('/cats', (req, res) => {
+  res.send('meow')
+})
+
+app.get('/dogs', (req, res) => {
+  res.send('woof')
+})
+
+// POSTs
+app.post('/cats', (req, res) => {
+  res.send('This is a post request to /cats')
+})
+
+// 404 handling, always at the end of the routes
+app.get('*', (req, res) => {
+  res.send('error 404, path not found')
 })
