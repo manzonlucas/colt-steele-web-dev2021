bee89c616ea28b08a73c82e68fb1039657f9719e
manzonlucas <manzonlucasm@gmail.com>
2022-12-28 18:28:17 -0300

    express: query strings

--- 05 Node - Sections 30-35/firstApp/index.js
+++ 05 Node - Sections 30-35/firstApp/index.js
@@ -10,12 +10,12 @@ const app = express();
 // });
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('Server started on port 3000');
 })
 
 // GETs
 app.get('/', (req, res) => {
-  res.send('Welcome, this is home')
+  res.send('Hi! Welcome, this is home')
 })
 
 app.get('/cats', (req, res) => {
@@ -24,6 +24,11 @@ app.get('/cats', (req, res) => {
 
 app.get('/dogs', (req, res) => {
   res.send('woof')
+})
+
+// POST
+app.post('/cats', (req, res) => {
+  res.send('This is a post request to /cats')
 })
 
 // GET: path parameter
@@ -38,10 +43,15 @@ app.get('/r/:subreddit/:postId', (req, res) => {
   res.send(`Hey! This is the post ID: ${postId} on the ${subreddit} subreddit`)
 })
 
-// POSTs
-app.post('/cats', (req, res) => {
-  res.send('This is a post request to /cats')
+// Query strings
+app.get('/search', (req, res) => {
+  const { q } = req.query;
+  res.send(`Search results for: ${q}`)
 })
+// URL for setting q value: http://localhost:3000/search?q=dog
+
+// With query strings with more than 1 key-value
+// In URL separate keys with an &: http://localhost:3000/search?q=dog&color=red
 
 // 404 handling, always at the end of the routes
 app.get('*', (req, res) => {
